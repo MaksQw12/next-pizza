ef50fe8d0c1de57f3ba283cf64845f5145466f4e
admin <Maksim7142@yandex.ru>
2024-09-21 08:56:13 +0300

    updated Prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,9 +1,17 @@
 import { prisma } from '@/prisma/prisma-client';
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET() {
   const users = await prisma.user.findMany();
   return NextResponse.json(users);
 }
 
-export async function POST() {}
+export async function POST(Req: NextRequest) {
+  const data = await Req.json();
+
+  const user = await prisma.user.create({
+    data,
+  });
+
+  return NextResponse.json(user);
+}
